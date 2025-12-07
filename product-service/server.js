6a91d78ca2317568aa3c1a9fe192fbd50e27f7c0
MOHAMED <mohamedlefi2004@gmail.com>
2025-12-07 19:49:34 +0100

    Update server.js

--- product-service/server.js
+++ product-service/server.js
@@ -1,1 +1,24 @@
 
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+
+// DB Mongo
+const db = require("./db/mongo");
+
+// Routes products
+const serviceRoutes = require("./routes/services");
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/services", serviceRoutes);
+
+// Start service
+const PORT = process.env.PORT || 4002;
+app.listen(PORT, () => {
+  console.log(Product Service running on port ${PORT});
+});
