2f147ea4cbc8462a190efc15695e843a1d352bba
MOHAMED <mohamedlefi2004@gmail.com>
2025-12-07 19:24:48 +0100

    Delete backend directory , frontend directory , product-service directory

--- backend/db/mongo.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://localhost:27017/mydb")
-  .then(() => console.log("Mongo connected"))
-  .catch(err => console.log(err));
-
-module.exports = mongoose;
