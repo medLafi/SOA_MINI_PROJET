ad509b6423864407b420b35f359092c31eca8cdb
MOHAMED <mohamedlefi2004@gmail.com>
2025-12-07 19:50:43 +0100

    Update server.js

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -1,13 +1,33 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
 
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
 app.use(cors());
 app.use(express.json());
 
-// importer les routes
-app.use(require("./routes/auth"));
-app.use(require("./routes/users"));
-app.use(require("./routes/services"));
+// USER-SERVICE
+app.use(
+  "/users",
+  createProxyMiddleware({
+    target: "http://user-service:4001",
+    changeOrigin: true,
+  })
+);
 
-app.listen(3000, () => console.log("Server running"));
+// PRODUCT-SERVICE
+app.use(
+  "/services",
+  createProxyMiddleware({
+    target: "http://product-service:4002",
+    changeOrigin: true,
+  })
+);
+
+// Start gateway
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(API Gateway running on port ${PORT});
+});
